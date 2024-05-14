c5a4e386f682bc75d467edd4dd8369c06500b910
Martin Javier Di Liscia <mjdiliscia@gmail.com>
2024-05-15 00:53:59 +0200

    Implemented a very basic cache for http retrieved pixbufs.

--- utils/httpWorker.go
+++ utils/httpWorker.go
@@ -3,32 +3,53 @@ package utils
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gotk3/gotk3/gdk"
 )
 
+var httpCache map[string]*gdk.Pixbuf
+
 func LoadPixmapFromUrl(url string) (pixbuf *gdk.Pixbuf, err error) {
-	response, err := http.Get(url)
-	if err != nil {
-		return
+	if httpCache == nil {
+		httpCache = make(map[string]*gdk.Pixbuf)
 	}
 
-	if response.StatusCode != 200 {
-		return nil, fmt.Errorf("%s", response.Status)
+	timestamp := time.Now()
+	pixbuf, ok := httpCache[url]
+	if !ok {
+		var response *http.Response
+		response, err = http.Get(url)
+		if err != nil {
+			return
+		}
+
+		if response.StatusCode != 200 {
+			return nil, fmt.Errorf("%s", response.Status)
+		}
+
+		var data []byte
+		data, err = io.ReadAll(response.Body)
+		if err != nil {
+			return
+		}
+
+		var loader *gdk.PixbufLoader
+		loader, err = gdk.PixbufLoaderNew()
+		if err != nil {
+			return
+		}
+
+		pixbuf, err = loader.WriteAndReturnPixbuf(data)
+		log.Printf("GET time for '%s': %d", url, time.Now().Sub(timestamp).Milliseconds())
+
+		httpCache[url] = pixbuf
+	} else {
+		log.Printf("CACHE time for '%s': %d", url, time.Now().Sub(timestamp).Milliseconds())
 	}
 
-	data, err := io.ReadAll(response.Body)
-	if err != nil {
-		return
-	}
-
-	loader, err := gdk.PixbufLoaderNew()
-	if err != nil {
-		return
-	}
-
-	pixbuf, err = loader.WriteAndReturnPixbuf(data)
 	return
 }
 
